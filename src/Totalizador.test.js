217d686496255fa46b9f18ee6c68d8b2415c2646
Diego Quinones <diegoqr2000@gmail.com>
2021-10-01 16:15:06 -0400

    Ahora calcularTotal admite todos los estados

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -1,23 +1,30 @@
-describe("Totalizador ", () => {
+describe("Totalizador-Cantidad", () => {
   it("deberia calcular para un cantidad", () => {
     expect(calcularTotal(2, 1, "")).toEqual(2);
   });
   it("deberia calcular para un cantidad", () => {
     expect(calcularTotal(2, 2, "")).toEqual(4);
   });
-  it("deberia calcular para un estado por defecto", () => {
+  it("deberia calcular para el estado de California", () => {
     expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
   });
-  it("deberia calcular para un estado por defecto", () => {
-    expect(calcularTotal(3, 2, "CA")).toEqual(6.495);
+  it("deberia calcular para el estado de Nevada", () => {
+    expect(calcularTotal(3, 2, "NV")).toEqual(6.48);
   });
-  it("deberia calcular para un estado por defecto", () => {
+  it("deberia calcular para un estado de Utah", () => {
     expect(calcularTotal(3, 2, "UT")).toEqual(6.399);
+  });
+  it("deberia calcular para un estado de Texas", () => {
+    expect(calcularTotal(3, 2, "TX")).toEqual(6.375);
+  });
+  it("deberia calcular para un estado de Alabama", () => {
+    expect(calcularTotal(3, 2, "AL")).toEqual(6.24);
   });
 });
 
+
 function impuestoEstado(estado) {
-  let impuestos = { CA: 0.0825, UT: 0.0665, "": 0 };
+  let impuestos = { CA: 0.0825, UT: 0.0665, NV: 0.08, TX: 0.0625, AL: 0.04,"": 0 };
   return impuestos[estado];
 }
 
